1f827e1f1e047d29d8e1611e7838809833df375e
s.onodera <17252514+onodera0314@users.noreply.github.com>
2024-08-26 20:41:04 +0900

    Update wezterm.lua

--- wezterm.lua
+++ wezterm.lua
@@ -5,22 +5,50 @@ if wezterm.target_triple == "x86_64-pc-windows-msvc" then
     config.default_prog = {"wsl.exe", "~", "-d", "ubuntu"}
 end
 
+config.automatically_reload_config = true
+config.use_ime = true
+config.audible_bell = "Disabled"
+
 config.font = wezterm.font("Cica")
 config.font_size = 14
-config.color_scheme = "Blazer"
-config.use_ime = true
+config.color_scheme = "Tokyo Night Moon"
 
 config.leader = { key = 'b', mods = 'CTRL', timeout_milliseconds = 1000 }
 config.keys = {
     {
-        key = "v",
+        key = "h",
         mods = "LEADER",
         action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" }
     },
     {
-        key = "h",
+        key = "v",
         mods = "LEADER",
         action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" }
+    },
+    {
+        key = "LeftArrow",
+        mods = "LEADER",
+        action = wezterm.action.ActivatePaneDirection "Left"
+    },
+    {
+        key = "RightArrow",
+        mods = "LEADER",
+        action = wezterm.action.ActivatePaneDirection "Right"
+    },
+    {
+        key = "UpArrow",
+        mods = "LEADER",
+        action = wezterm.action.ActivatePaneDirection "Up"
+    },
+    {
+        key = "DownArrow",
+        mods = "LEADER",
+        action = wezterm.action.ActivatePaneDirection "Down"
+    },
+    {
+        key = "o",
+        mods = "LEADER",
+        action = wezterm.action.PaneSelect
     },
     {
         key = "s",
