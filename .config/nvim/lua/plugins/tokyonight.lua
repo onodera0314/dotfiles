b914b5571a901fd4d0aa52e08d1d0bb88611a198
onodera0314 <17252514+onodera0314@users.noreply.github.com>
2026-02-17 18:00:07 +0900

    背景を透過させる

--- .config/nvim/lua/plugins/tokyonight.lua
+++ .config/nvim/lua/plugins/tokyonight.lua
@@ -1,7 +1,18 @@
 return {
   'folke/tokyonight.nvim',
   lazy = false,
-  config = function()
+  opts = {
+    transparent = true,
+    styles = {
+      sidebars = "transparent",
+      floats = "transparent"
+    }
+  },
+  config = function(_, opts)
+    require("tokyonight").setup(opts)
     vim.cmd([[colorscheme tokyonight-moon]])
+    -- 背景を透過
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
   end
 }
